01b6b3ae87715fa465f3e22b250c4b6245721d1a
nuryadincjr <nuryadin.cjr@gmail.com>
2023-09-24 10:47:10 +0700

    fix code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,61 @@
 'use strict';
 
-// Fungsi untuk toggle elemen dengan kelas "active"
-const toggleElementActive = function (elem) {
-  elem.classList.toggle("active");
-}
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-// Sidebar variables
+// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// Sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () {
-  toggleElementActive(sidebar);
-});
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-// Custom select variables
+// custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-select-value]");
+const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () {
-  toggleElementActive(this);
-});
+select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// Event listener untuk semua item dalam select
+// add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
+
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
-    toggleElementActive(select);
+    elementToggleFunc(select);
     filterFunc(selectedValue);
+
   });
 }
 
-// Filter variables
+// filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
+
   for (let i = 0; i < filterItems.length; i++) {
-    if (selectedValue === "all" || selectedValue === filterItems[i].dataset.category) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
     }
+
   }
+
 }
 
-// Event listener untuk semua tombol filter pada layar besar
+// add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
+
   filterBtn[i].addEventListener("click", function () {
+
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
@@ -59,31 +63,35 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
+
   });
+
 }
 
-// Variabel formulir kontak
+// // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// Event listener untuk semua bidang input dalam formulir
+// add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-    // Memeriksa validitas formulir
+
+    // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
+
   });
 }
 
-// Variabel navigasi halaman
+// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// Event listener untuk semua tautan navigasi
+// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     for (let j = 0; j < pages.length; j++) {
@@ -102,7 +110,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-// Fungsi untuk memeriksa hash fragment saat halaman dimuat
+// checks the fragment hash when the page loads
 function checkInitialHash() {
   const hash = window.location.hash.slice(1);
 
@@ -121,7 +129,7 @@ function checkInitialHash() {
   }
 }
 
-// Panggil fungsi checkInitialHash saat halaman dimuat
+// call the checkInitialHash function when the page loads
 window.addEventListener("load", checkInitialHash);
 
 if (!window.location.hash) {
